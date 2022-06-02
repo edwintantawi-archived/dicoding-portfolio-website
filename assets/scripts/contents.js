e29a4090d403c1730b6d1213c2fcfe6a77e5f085
edwintantawi <edwintantawi.workspace@gmail.com>
2022-06-02 23:13:42 +0700

    add footer and content rendering

---
+++ assets/scripts/contents.js
@@ -0,0 +1,104 @@
+const projects = [
+    {
+        title: 'Todonesia',
+        category : 'TodoList',
+        tags: ['React JS', 'Firebase'],
+        url: 'https://github.com/edwintantawi/projects__todonesia'
+    },
+    {
+        title: 'AirBnB Clone',
+        category : 'Clone App',
+        tags: ['Next JS', 'Tailwindcss'],
+        url: 'https://github.com/edwintantawi/clone-app__airbnb'
+    },
+    {
+        title: 'DevNote',
+        category : 'Note App',
+        tags: ['Dart', 'Flutter'],
+        url: 'https://github.com/edwintantawi/devnote_app'
+    },
+];
+
+const blogs = [
+    {
+        title: 'Mengenal Teknologi Web-Components',
+        tags: ['Web Component', 'Javascript', 'HTML'],
+        summary: 'Pernahkah kamu membanyangkan membuat element HTML kita sendiri? Wah kalau bisa itu sungguh luar biasa bukan?',
+        url: 'https://edwintantawi.medium.com/mengenal-teknologi-web-component-301a276b966c',
+        date: '2022-05-16',
+    },
+    {
+        title: 'Yang tidak kamu ketahui tentang javascript, serta bedah increment operator',
+        tags: ['Conquer Javascript','Javascript'],
+        summary: 'Mengulik beberapa hal yang mungkin saja kita tidak ketahui di javascript, serta membeda salah satu operator yaitu incement operator',
+        url: 'https://edwintantawi.medium.com/conquer-javascript-1-berteman-dengan-javascript-cb226192eabf',
+        date: '2021-10-11',
+    },
+    {
+        title: 'Mengenal TailwindCSS v2.2.10',
+        tags: ['CSS', 'Tailwindcss'],
+        summary: 'TailwindCSS merupakan framework CSS(Cascading Style Sheets) yang menganut konsep Utility-First. Kita akan belajar menguasai TailwindCSS dari basicnya',
+        url: 'https://edwintantawi.medium.com/belajar-tailwindcss-v2-2-10-a4584c64020c',
+        date: '2021-10-08',
+    },
+];
+
+function createProjectTemplate(project){
+    const {title, category, tags, url} = project;
+
+    return `
+        <a href="${url}">
+            <article class="card project">
+                <img src="assets/images/brand-logo.svg" alt="" class="project__image">
+                <header>
+                    <h3 class="project__heading">${title} <span class="project__category">(${category})</span></h3>
+                    <ul class="tags">
+                        ${tags.map((tag) => {
+                            return `<li class="tags__item">${tag}</li>`    
+                        }).join('')}                        
+                    </ul>
+                </header>
+            </article>
+        </a>
+    `;
+}
+
+function createBlogTemplate(blog) {
+    const {title, tags, summary, url, date} = blog;
+
+    return `
+        <a href="${url}">
+            <article class="blog">
+                <header>
+                    <h3 class="blog__heading">${title}</h3>
+                    <ul class="tags">
+                        ${tags.map((tag) => {
+                            return `<li class="tags__item">${tag}</li>`
+                        }).join('')}
+                    </ul>
+                </header>
+
+                <p class="blog__summary mt-2">
+                    <time datetime="${date}" class="blog__date">${date}</time> • ${summary}
+                </p>
+            </article>
+        </a>
+    `;
+}
+
+function getYearsRangeFrom(yearFrom) {
+    const currentYear = new Date().getFullYear();
+    const isSameYear = currentYear === yearFrom;
+
+    if(isSameYear) return yearFrom;
+
+    return `${yearFrom} - ${currentYear}`
+}
+
+const projectContent = document.getElementById('project-contents');
+const blogContent = document.getElementById('blog-contents');
+const copyrightYearsContent = document.getElementById('copyright-years');
+
+projects.forEach((project) => projectContent.innerHTML += createProjectTemplate(project));
+blogs.forEach((blog) => blogContent.innerHTML += createBlogTemplate(blog));
+copyrightYearsContent.innerText = getYearsRangeFrom(2021)
