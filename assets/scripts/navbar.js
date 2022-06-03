f8ca2ab4471820fa0e73b0f837bdc6b57587ce9f
edwintantawi <edwintantawi.workspace@gmail.com>
2022-06-03 12:20:11 +0700

    add action get in touch and mobile nav js

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -1,13 +1,17 @@
 const navbarToggler = document.getElementById('navbar-toggler');
 const navbarNav = document.getElementById('navbar-nav');
-
-navbarToggler.addEventListener('click', toggleHandler)
+const navbar = document.getElementById('navbar');
+const navLinks = document.getElementsByClassName('navbar__link');
 
 function toggleHandler(){
     const TOGGLE_OPEN_CLASS = 'navbar__toggler--open';
     const NAV_OPEN_CLASS = 'navbar__nav--open';
-
+    const NAVBAR_OPEN_CLASS = 'navbar--open';
 
     navbarToggler.classList.toggle(TOGGLE_OPEN_CLASS);
     navbarNav.classList.toggle(NAV_OPEN_CLASS)
+    navbar.classList.toggle(NAVBAR_OPEN_CLASS)
 }
+
+navbarToggler.addEventListener('click', toggleHandler)
+Array(...navLinks).forEach((navLink) => navLink.addEventListener('click', toggleHandler))
